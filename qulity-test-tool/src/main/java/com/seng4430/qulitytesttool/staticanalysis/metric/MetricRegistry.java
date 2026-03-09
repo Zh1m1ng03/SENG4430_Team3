7227eb97815870269033d6f93cb4d2d3160d097f
zhiyeren003 <zhiyeren003@gmail.com>
2026-03-09 21:24:09 +1100

    UncaughtExceptionAnalyser

--- qulity-test-tool/src/main/java/com/seng4430/qulitytesttool/staticanalysis/metric/MetricRegistry.java
+++ qulity-test-tool/src/main/java/com/seng4430/qulitytesttool/staticanalysis/metric/MetricRegistry.java
@@ -2,6 +2,7 @@ package com.seng4430.qulitytesttool.staticanalysis.metric;
 
 import com.seng4430.qulitytesttool.shared.config.AnalysisConfig;
 import com.seng4430.qulitytesttool.staticanalysis.metric.impl.CyclomaticComplexityAnalyser;
+import com.seng4430.qulitytesttool.staticanalysis.metric.impl.UncaughtExceptionAnalyser;
 
 import java.util.List;
 
@@ -13,7 +14,8 @@ public class MetricRegistry {
 
     public static List<MetricAnalyser> getAll(AnalysisConfig config) {
         return List.of(
-                new CyclomaticComplexityAnalyser(config.getCyclomaticComplexity())
+                new CyclomaticComplexityAnalyser(config.getCyclomaticComplexity()),
+                new UncaughtExceptionAnalyser()
         );
     }
 }
